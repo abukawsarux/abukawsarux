b1ac7ddae095ba0341488c8d874d0aca87c2f27b
uiabu <ui.abukawsar@gmail.com>
2025-01-09 09:10:34 +0600

    remove unused skills from MySkills component

--- components/home/MySkills.jsx
+++ components/home/MySkills.jsx
@@ -31,10 +31,7 @@ const skillsInfo = [
     icon: "/assets/skillicon/typescript.png",
     title: "TypeScript",
   },
-  {
-    icon: "/assets/skillicon/python.png",
-    title: "Python",
-  },
+
   {
     icon: "/assets/skillicon/reactjs.png",
     title: "React Js",
@@ -49,70 +46,6 @@ const skillsInfo = [
   },
 
   {
-    icon: "/assets/skillicon/reactnative.png",
-    title: "React Native",
-  },
-  {
-    icon: "/assets/skillicon/redux.png",
-    title: "Redux",
-  },
-  {
-    icon: "/assets/skillicon/nodejs.png",
-    title: "Node Js",
-  },
-  {
-    icon: "/assets/skillicon/js.png",
-    title: "Express Js",
-  },
-  {
-    icon: "/assets/skillicon/django.png",
-    title: "Django",
-  },
-  {
-    icon: "/assets/skillicon/restapi.png",
-    title: "Rest Api",
-  },
-  {
-    icon: "/assets/skillicon/graphql.png",
-    title: "GraphQL",
-  },
-  {
-    icon: "/assets/skillicon/apollowhite.png",
-    title: "Apollo",
-  },
-  {
-    icon: "/assets/skillicon/mongodb.png",
-    title: "MongoDB",
-  },
-  {
-    icon: "/assets/skillicon/mysql.png",
-    title: "MySql",
-  },
-  {
-    icon: "/assets/skillicon/postgresql.png",
-    title: "PostgreSql",
-  },
-  {
-    icon: "/assets/skillicon/prismaiconwhite.png",
-    title: "Prisma",
-  },
-  {
-    icon: "/assets/skillicon/websocketwhite.png",
-    title: "WebSocket",
-  },
-  {
-    icon: "/assets/skillicon/awswhite.png",
-    title: "AWS",
-  },
-  {
-    icon: "/assets/skillicon/docker.png",
-    title: "Docker",
-  },
-  {
-    icon: "/assets/skillicon/git.png",
-    title: "Git",
-  },
-  {
     icon: "/assets/skillicon/github.png",
     title: "Github",
   },
@@ -120,10 +53,7 @@ const skillsInfo = [
     icon: "/assets/skillicon/framerwhite.png",
     title: "Framer",
   },
-  {
-    icon: "/assets/skillicon/postman.png",
-    title: "Postman",
-  },
+
   {
     icon: "/assets/skillicon/figma.png",
     title: "Figma",
